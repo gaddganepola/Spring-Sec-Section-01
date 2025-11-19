fcccb01982a224c551a12bee63db2baeb837344a
gaddganepola <ganeypola@gmail.com>
2025-11-19 07:52:39 +0530

    initial commit

---
+++ src/main/java/com/ddg/SpringSecSection01/SpringSecSection01Application.java
@@ -0,0 +1,14 @@
+package com.ddg.SpringSecSection01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecSection01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecSection01Application.class, args);
+
+	}
+
+}

126a1a931ff431e70081132b124563a807589245
gaddganepola <ganeypola@gmail.com>
2025-11-19 08:45:09 +0530

    feature: welcome endpoint

--- src/main/java/com/ddg/SpringSecSection01/SpringSecSection01Application.java
+++ src/main/java/com/ddg/SpringSecSection01/SpringSecSection01Application.java
@@ -2,8 +2,12 @@ package com.ddg.SpringSecSection01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+
+//This is required if I create the controller package outside my main package.
+//@ComponentScan("com.ddg.SpringSecSection01.controller")
 public class SpringSecSection01Application {
 
 	public static void main(String[] args) {

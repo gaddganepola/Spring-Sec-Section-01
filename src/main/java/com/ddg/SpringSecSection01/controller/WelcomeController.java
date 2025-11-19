7e1ca363af2173915db6481c683bfb3716b91392
Dinethra Dayan <153478817+gaddganepola@users.noreply.github.com>
2025-11-19 09:02:22 +0530

    Add comment for welcome API endpoint
    Added a comment to clarify the purpose of the welcome endpoint.

--- src/main/java/com/ddg/SpringSecSection01/controller/WelcomeController.java
+++ src/main/java/com/ddg/SpringSecSection01/controller/WelcomeController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController {
 
+    // REST API endpoint for welcome
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to Spring Security";
